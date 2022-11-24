dedc228a81870269dd600363b6adab38894a927f
RussianT4werka <sanya.kokorin.04@mail.ru>
2022-11-24 15:23:38 +1000

    комит1

--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -3,12 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ООО__Столовые_приборы_.Models;
 using ООО__Столовые_приборы_.Tools;
+using ООО__Столовые_приборы_.Views;
 
 namespace ООО__Столовые_приборы_.ViewModel
 {
-    public class MainWindowVM: BaseVM
+    public class MainWindowVM : BaseVM
     {
-    
+        private Page currentPage;
+
+        private Visibility loggedIn1;
+        private User user = new User();
+
+        public Command Logout { get; set; }
+
+        public Page CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                SignalChanged();
+            }
+        }
+
+        public User User 
+        {
+            get => user;
+            set
+            {
+                LoggedIn = Visibility.Visible;
+                user = value;
+                SignalChanged();
+                SignalChanged("UserName");
+                SignalChanged("Role");
+            }
+        }
+
+
+        public Visibility LoggedIn
+        {
+            get => loggedIn1;
+            set
+            {
+                loggedIn1 = value;
+                SignalChanged();
+            }
+
+        }
+
+        public MainWindowVM()
+        {
+            CurrentPage = new AuthPage(this);
+
+            Logout = new Command(() =>
+            {
+
+            });
+        }
     }
 }
---
+++ Views/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ООО__Столовые_приборы_.Views
+{
+    public class Command: ICommand
+    {
+        Action action;
+        public Command(Action action)
+        {
+            this.action = action;
+        }
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            action?.Invoke();
+        }
+    }
+}
